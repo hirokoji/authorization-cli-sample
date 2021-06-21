8c6365e7257096f9030a9d609ebb79e4fb70d9c5
Hiro Kojima <guitarsaiko@gmail.com>
2021-06-21 14:32:22 +0900

    Add jwt access token genate/verify logic

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ ssh-keygen -t rsa -b 4096 -m PEM -f keys/jwtRS256.key
 openssl rsa -in keys/jwtRS256.key -pubout -outform PEM -out keys/jwtRS256.key.pub
 ```
 
-
 ## Usage
 
 ```shell script
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello World');
+import * as fs from 'fs';
+import { sign, verify } from 'jsonwebtoken';
+
+const main = async () => {
+    const keyDir = `${__dirname}/../keys`;
+    const privateKeyName = `jwtRS256.key`;
+    const publicKeyName = `jwtRS256.key.pub`;
+
+    const generateToken = async (): Promise<string> => {
+        const privateKey = await fs.promises.readFile(`${keyDir}/${privateKeyName}`);
+        return sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' });
+    };
+
+    const verifyToken = async (token: string): Promise<boolean> => {
+        const publicKey = await fs.promises.readFile(`${keyDir}/${publicKeyName}`);
+        try {
+            verify(token, publicKey);
+            return true;
+        } catch (e) {
+            if (e instanceof Error) {
+                console.error('Error: ', e.message);
+            } else {
+                console.error('Error: ', e);
+            }
+            return false;
+        }
+    };
+
+
+    const token = await generateToken();
+    if(await verifyToken(token)){
+	    console.error('Succeeded to generate/verify the access token.');
+    }
+};
+
+main();
